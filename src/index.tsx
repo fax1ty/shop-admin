4e3bc02a73404131186e6be82daf0ee402ef057a
Artemiy Davydov <artemiy.davydov@mercury.office>
2022-04-07 01:22:33 +0400

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="app">Everything is working</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
